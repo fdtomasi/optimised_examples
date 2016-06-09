ebec8a6159580a8bfc3ba8daa7fbda57e07a0533
Federico T <fdtomasi@gmail.com>
2016-06-09 17:17:50 +0200

    Update myff.py

--- myff.py
+++ myff.py
@@ -1,14 +1,23 @@
+"""This shows a comparison in using an hamming distance on a 
+couple of strings of the same length. When strings are long,
+it is easy to see how the conversion from strings to list is inefficient.
+
+Usage:
+    python myff.py
+"""
+
 import numpy as np
 from time import time
 
 n = 1000
 l1 = ['AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHBBBBS']*n
 l2 = ['AAAAAAAAAAAAAffffffffffffffAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHBBBBS']*n
+
 def f():
 	t = time()
 	for i in l1:
 		for j in l2:
-			(np.array(list(i)) != np.array(list(j))).mean()
+            (np.array(list(i)) != np.array(list(j))).mean()
 	print "Converting to list then array:", time()-t
 
 def f2(ntype=np.int8):
